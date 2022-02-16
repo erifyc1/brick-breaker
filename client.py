4373188629a4494ffb2f88b0d4272d6925503baf
Jacob Stolker <stolkerj@yahoo.com>
2022-02-16 14:02:58 -0600

    added ball with buggy bounce mechanic

--- client.py
+++ client.py
@@ -1,8 +1,10 @@
 import pygame
 import random
+import math
 
 aspect = (800, 600)
 player_size = (100, 50)
+ball_size = (25,25)
 barrier_size = (50,25)
 color_gradient = [(150, 0, 0), (150, 100, 0), (0, 150, 0), (0, 50, 150), (75, 0, 75)]
 
@@ -33,18 +35,42 @@ class Barrier():
         pygame.draw.rect(win,self.color, self.rect)
 
 class Ball():
-    def __init__(self, s, width, height, color):
-        self.x = s
-        self.y = s
-        self.width = width
-        self.height = height
+    def __init__(self, x, y, side, color):
+        self.x = x
+        self.y = y
+        self.width = side
+        self.height = side
         self.color = color
-        self.rect = (s,s,width,height)
-        self.dir = randint(-70, 70)
+        self.rect = (x,y,side,side)
+        self.dir = math.pi*(random.random()-0.5)
+        self.vel = 3
     def draw(self, win):
         pygame.draw.rect(win,self.color, self.rect)
-    def bounce():
-        d = 0
+    def bounce(self, edge_dir):
+        #self.dir = math.acos(-math.cos(dir-edge_dir)) + math.asin(-math.sin(dir-edge_dir))
+        self.dir -= math.pi/2
+    def move(self):
+        x_diff = self.vel*math.cos(self.dir)
+        y_diff = self.vel*math.sin(self.dir)
+        if self.x + x_diff < 0:
+            self.x = 0
+            self.bounce(0)
+        elif self.x + x_diff > aspect[0]-ball_size[0]:
+            self.x = aspect[0]-ball_size[0]
+            self.bounce(0)
+        else:
+            self.x += self.vel*math.cos(self.dir)
+        
+        if self.y + y_diff < 0:
+            self.y = 0
+            self.bounce(math.pi/2)
+        elif self.y + y_diff > aspect[1]-ball_size[1]:
+            self.y = aspect[1]-ball_size[1]
+            self.bounce(math.pi/2)
+        else:
+            self.y += self.vel*math.sin(self.dir)
+        
+        self.rect = (self.x, self.y, self.width, self.height)
 
 
 class Player():
@@ -76,9 +102,10 @@ class Player():
         self.rect = (self.x, self.y, self.width, self.height)
 
 
-def redrawWindow(win, player, barriers):
+def redrawWindow(win, player, barriers, ball):
     win.fill((0,0,0))
     player.draw(win)
+    ball.draw(win)
     for b in barriers:
         b.draw(win)
     pygame.display.update()
@@ -87,6 +114,8 @@ def main():
     run = True
     p = Player(aspect[0]/2-(player_size[0]/2),aspect[1]-(player_size[1]/2),player_size[0],player_size[1],(200,200,200))
     barriers = init_barriers(barrier_size)
+    ball = Ball(aspect[0]/2,aspect[1]/2,25,(200,200,200))
+
     clock = pygame.time.Clock()
     while run:
         clock.tick(60)
@@ -96,7 +125,8 @@ def main():
                 pygame.quit()
         
         p.move()
-        redrawWindow(win, p, barriers)
+        ball.move()
+        redrawWindow(win, p, barriers, ball)
 
 
 
